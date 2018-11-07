8be2669df828f5ebc6e1df34302e9ddd3e746259
lhqing <liuhq@ucsd.edu>
2018-11-06 22:54:43 -0800

    update qsub check

---
+++ cemba_data/local/__init__.py
@@ -0,0 +1,4 @@
+"""
+File include:
+- qsub.py
+"""
--- cemba_data/local/qsub.py
+++ cemba_data/local/qsub.py
@@ -8,16 +8,8 @@ import sys
 import time
 import argparse
 
-qsub_config = configparser.ConfigParser()
-qsub_config.read(os.path.dirname(__file__) + '/config_qsub.ini')
 
-
-def print_config():
-    print(json.dumps(qsub_config._sections, indent=4))
-    return
-
-
-def default_command_dict(name, error_path, output_path):
+def default_command_dict(name, error_path, output_path, qsub_config):
     command_dict = {'command': None,
                     '-N': name,
                     '-V': '',
@@ -30,8 +22,8 @@ def default_command_dict(name, error_path, output_path):
     return command_dict
 
 
-def check_qstat(id_set=None):
-    qstat_result_string = run(['qstat', '-u', qsub_config['USER']['USER_NAME']],
+def get_running_job_id_qstat(user_name, id_set):
+    qstat_result_string = run(['qstat', '-u', user_name],
                               stdout=PIPE, encoding='utf8').stdout
     if 'job-ID' not in qstat_result_string:
         print('qstat nothing')
@@ -51,86 +43,73 @@ def check_qstat(id_set=None):
 
 class Qsubmitter:
     def __init__(self, command_file_path, project_name,
-                 auto_submit=False, qacct=False, resubmit=None,
-                 total_cpu=None, submission_gap=None, qstat_gap=None):
+                 total_cpu=60, submission_gap=2, qstat_gap=30):
+        # prepare config
+        self.qsub_config = configparser.ConfigParser()
+        self.qsub_config.read(os.path.dirname(__file__) + '/config_qsub.ini')
+
+        # prepare job_dir
+        job_dir = self.qsub_config['QSUB_DEFAULT']['JOB_DIR']
+        working_job_dir = job_dir + '/' + project_name
+        try:
+            os.mkdir(working_job_dir)
+        except OSError:
+            print('Note: The job has been submitted before.')
+            print(f'If want to resubmit everything, delete or rename this directory: {working_job_dir}')
+            sys.stdout.flush()
+        run(['cp', command_file_path, working_job_dir])
+
+        # submission parameters
+        self.total_cpu = total_cpu
+        self.submission_gap = submission_gap
+        self.qstat_gap = qstat_gap
+
+        # auto detect user name
+        self.user_name = run(['whoami'], stdout=PIPE, encoding='utf8').stdout.strip()
+
+        # prepare project dir
         self.command_file_path = command_file_path
         self.project_name = project_name
-        self.project_dir = qsub_config['QSUB_DEFAULT']['JOB_DIR'] + '/' + project_name
+        self.project_dir = self.qsub_config['QSUB_DEFAULT']['JOB_DIR'] + '/' + project_name
         if not os.path.exists(self.project_dir):
             os.mkdir(self.project_dir)
 
+        # get all Command objects
         self.commands = self.parse_command_file()
+
+        # initiate running stats
         self.running_commands = []
         self.running_cpu = 0
         self.submitted_qsub_id = set()
         self.submission_fail_commands = []
-        self.finished_commands = []  # modify this with check_command_finish_status
-        self.qsub_failed_commands = []  # modify this with check_command_finish_status
-        self.cmd_failed_commands = []  # modify this with check_command_finish_status
-        self.check_qacct = qacct  # check qacct is slow if the log is too large, only check it in debugging
+        self.finished_commands = []
         self.finish_count = 0
-        if total_cpu is None:
-            self.total_cpu = int(qsub_config['RUNNING_DEFAULT']['TOTAL_CPU'])
-        else:
-            self.total_cpu = total_cpu
-        if submission_gap is None:
-            self.submission_gap = int(qsub_config['RUNNING_DEFAULT']['SUBMISSION_GAP'])
-        else:
-            self.submission_gap = submission_gap
-        if qstat_gap is None:
-            self.qstat_gap = int(qsub_config['RUNNING_DEFAULT']['QSTST_GAP'])
-        else:
-            self.qstat_gap = qstat_gap
 
-        if auto_submit:
-            self.submit(resubmit)
+        # submit jobs
+        self.submit()
         return
 
     def parse_command_file(self):
         with open(self.command_file_path) as command:
             command_dict_list = json.load(command)
             obj_list = []
-            n = 0
-            for command_dict in command_dict_list:
+            for n, command_dict in enumerate(command_dict_list):
                 obj_list.append(Command(command_dict=command_dict,
                                         unique_id=f'{self.project_name}_{n}',
-                                        project_dir=self.project_dir))
-                n += 1
+                                        project_dir=self.project_dir,
+                                        qsub_config=self.qsub_config))
         return obj_list
 
-    def submit(self, resubmit=None):
+    def submit(self):
         # job submission process:
         for command_obj in self.commands:
-            # judge if command has been submitted
-            if command_obj.submitted:
-                if resubmit is None:
-                    # print('Skip command:', command_obj.unique_id)
-                    self.check_command_finish_status(command_obj)
-                    continue  # not resubmit command
-                elif resubmit == 'fail':
-                    # only resubmit failed command
-                    if not command_obj.qsub_failed and not command_obj.cmd_failed:
-                        # both a success, the job is success
-                        # print('Skip command:', command_obj.unique_id)
-                        self.check_command_finish_status(command_obj)
-                        continue
-                    else:
-                        print('Resubmit failed command:', command_obj.unique_id)
-                        sys.stdout.flush()
-                elif resubmit == 'all':
-                    # resubmit all command
-                    pass
-                else:
-                    raise ValueError('resubmit only allow None, fail or all. Got', resubmit)
-
             command_cpu = int(command_obj.qsub_parameter['-pe smp'])
-            if self.running_cpu + command_cpu > self.total_cpu:
+            if (self.running_cpu + command_cpu) > self.total_cpu:
                 while True:
                     time.sleep(self.qstat_gap)
                     self.check_running_cpu()
                     if self.running_cpu <= self.total_cpu:
                         break
-            # print('Submit job:', command_obj.unique_id)
             command_obj.submit()
             # gather submitted id and obj
             if command_obj.submission_fail:
@@ -151,26 +130,21 @@ class Qsubmitter:
         return
 
     def check_running_cpu(self):
-        # TODO: check result in the .error.log to see if there is any error
         print('check running job: ', end='')
-        sys.stdout.flush()
-        cur_running_qsub_id = check_qstat(self.submitted_qsub_id)
+        cur_running_qsub_id = get_running_job_id_qstat(user_name=self.user_name, id_set=self.submitted_qsub_id)
         # check every running obj
         cur_running_cpu = 0
         cur_running_command = []
         for command_obj in self.running_commands:
             if command_obj.qsub_id not in cur_running_qsub_id:
                 # command have finished
-                self.finish_count += 1
+                command_obj.finish = True
 
-                if self.check_qacct:
-                    print(command_obj.unique_id, 'qacct')
-                    sys.stdout.flush()
-                    if command_obj.query_qacct() != 0:
-                        raise ValueError('Command not finished but disappeared in qstat')
-                else:
-                    command_obj.not_query_qacct()
-                self.check_command_finish_status(command_obj)
+                # store the qsub id.
+                with open(command_obj.status_path, 'w') as f:
+                    json.dump({'qsub_id': command_obj.qsub_id}, f)
+                self.finish_count += 1
+                self.finished_commands.append(command_obj)
             else:
                 cur_running_command.append(command_obj)
                 cur_running_cpu += int(command_obj.qsub_parameter['-pe smp'])
@@ -181,65 +155,35 @@ class Qsubmitter:
         self.running_cpu = cur_running_cpu
         return
 
-    def check_command_finish_status(self, command_obj):
-        self.finished_commands.append(command_obj)
-        if self.check_qacct:
-            if command_obj.qsub_failed:
-                self.qsub_failed_commands.append(command_obj)
-            if command_obj.cmd_failed:
-                self.cmd_failed_commands.append(command_obj)
-        return
-
     def get_reports(self):
         cur_time = datetime.datetime.now().strftime("[%Y-%m-%d]%H:%M:%S")
         with open(self.project_dir + f'/submit_summary.{cur_time}.txt', 'w') as summary:
             summary.write(f'Total Command: {len(self.commands)}\n')
             summary.write(f'Submission Failed Command: {len(self.submission_fail_commands)}\n')
-
-            if self.check_qacct:
-                summary.write(f'Qsub Failed Command: {len(self.qsub_failed_commands)}\n')
-                if len(self.qsub_failed_commands) > 0:
-                    with open(self.project_dir + '/qsub_failed_commands.txt', 'w') as f:
-                        f.write('\n'.join(self.qsub_failed_commands))
-                    summary.write(f'See qsub failed command id in {self.project_dir}/qsub_failed_commands.txt\n')
-
-                summary.write(f'Command Failed Command: {len(self.cmd_failed_commands)}\n')
-                if len(self.cmd_failed_commands) > 0:
-                    with open(self.project_dir + '/cmd_failed_commands.txt', 'w') as f:
-                        f.write('\n'.join(self.cmd_failed_commands))
-                    summary.write(f'See qsub failed command id in {self.project_dir}/cmd_failed_commands.txt\n')
-            else:
-                summary.write('check_qacct = False, '
-                              'if want to get the finish code for each job, '
-                              'set qacct=True in Qsubmitter')
         return
 
 
 class Command:
-    def __init__(self, command_dict, unique_id, project_dir,
-                 error_path=None, output_path=None):
+    def __init__(self, command_dict, unique_id, project_dir, qsub_config):
         self.project_dir = project_dir
         self.unique_id = unique_id
+        self.qsub_config = qsub_config
 
         # command status
         self.submitted = False
         self.qsub_id = None
         self.submission_fail = False
         self.finish = False  # if submitted is True and not in qstat
-        self.qsub_failed = None  # check from qacct_status['failed'], fail due to qsub
-        self.cmd_failed = None  # check from qacct_status['exit_status'], fail due to cmd
-        self.qacct_status = None  # dict of qacct results
-        self.qacct_status_path = f'{self.project_dir}/{unique_id}.qacct.json'
+        self.status_path = f'{self.project_dir}/{unique_id}.json'
         self.script_path = f'{self.project_dir}/{unique_id}.sh'
-        if error_path is None:
-            self.error_path = f'{self.project_dir}/{unique_id}.error.log'
-        if output_path is None:
-            self.output_path = f'{self.project_dir}/{unique_id}.output.log'
+        self.error_path = f'{self.project_dir}/{unique_id}.error.log'
+        self.output_path = f'{self.project_dir}/{unique_id}.output.log'
 
         # prepare command dict
         self.command_dict = default_command_dict(name=self.unique_id,
                                                  error_path=self.error_path,
-                                                 output_path=self.output_path)
+                                                 output_path=self.output_path,
+                                                 qsub_config=self.qsub_config)
         self.command_dict.update(**command_dict)
         for k, v in self.command_dict.items():
             if v is None:
@@ -252,21 +196,6 @@ class Command:
 
         # make command sh file
         self.make_script_file()
-
-        # modify command status if it has been submitted and finished
-        if os.path.exists(self.qacct_status_path):
-            with open(self.qacct_status_path) as f:
-                status_dict = json.load(f)
-                if len(status_dict) > 1:
-                    self.qacct_status = status_dict
-                    self.qsub_id = self.qacct_status['jobnumber']
-                    self.judge_qacct_status()
-                else:  # didn't do qacct
-                    self.qsub_id = status_dict['qsub_id']
-                    # self.qacct_status is still None,
-                    # should run query_qacct to get it
-            self.submitted = True
-            self.finish = True
         return
 
     def make_script_file(self):
@@ -288,82 +217,16 @@ class Command:
         except AttributeError:
             self.submission_fail = True
         self.submitted = True
-
-        # remove previous qacct status
-        self.finish = False
-        self.qsub_failed = None
-        self.cmd_failed = None
-        self.qacct_status = None
-        return
-
-    def query_qacct(self):
-        if self.qacct_status is not None:
-            return 0
-        time.sleep(10)
-        qstat_result = run(['qstat', '-j', self.qsub_id], stdout=PIPE, stderr=PIPE, encoding='utf8')
-        if 'Following jobs do not exist' in qstat_result.stderr and self.submitted:
-            self.finish = True
-        else:
-            return 1  # not finish
-        qacct_result = run(['qacct', '-j', self.qsub_id, '-o', qsub_config['USER']['USER_NAME']],
-                           stdout=PIPE, encoding='utf8')
-        status_dict = {}
-        qacct_result_lines = qacct_result.stdout.split('\n')[1:-1]
-        for line in qacct_result_lines:
-            k = line[:13].strip()  # how to separate lines without \t???
-            v = line[13:].strip()
-            status_dict[k] = v
-        self.qacct_status = status_dict
-        self.judge_qacct_status()
-        with open(self.qacct_status_path, 'w') as f:
-            json.dump(self.qacct_status, f)
-        return 0
-
-    def not_query_qacct(self):
-        self.finish = True
-        # skip qacct, only store the qsub id.
-        with open(self.qacct_status_path, 'w') as f:
-            json.dump({'qsub_id': self.qsub_id}, f)
-
-    def judge_qacct_status(self):
-        if self.qacct_status['failed'] == '0':
-            self.qsub_failed = False
-        else:
-            self.qsub_failed = True
-        if self.qacct_status['exit_status'] == '0':
-            self.cmd_failed = False
-        else:
-            self.cmd_failed = True
         return
 
 
-def qsub(project_name, command_file_path, total_cpu=None, submission_gap=None, qstat_gap=None):
-    if total_cpu is None:
-        total_cpu = qsub_config['QSUB_DEFAULT']['TOTAL_CPU']
-    if submission_gap is None:
-        submission_gap = qsub_config['QSUB_DEFAULT']['SUBMISSION_GAP']
-    if qstat_gap is None:
-        qstat_gap = qsub_config['QSUB_DEFAULT']['QSTST_GAP']
-
-    job_dir = qsub_config['QSUB_DEFAULT']['JOB_DIR']
-    working_job_dir = job_dir+'/'+project_name
-    try:
-        os.mkdir(working_job_dir)
-    except OSError:
-        print('Note: The job has been submitted before.')
-        print(f'If want to resubmit everything, delete or rename this directory: {working_job_dir}')
-        sys.stdout.flush()
-    run(['cp', command_file_path, working_job_dir])
-
+def qsub(project_name, command_file_path, total_cpu=60, submission_gap=2, qstat_gap=30):
     submitter = Qsubmitter(command_file_path=command_file_path,
                            project_name=project_name,
-                           auto_submit=True,
                            total_cpu=total_cpu,
                            submission_gap=submission_gap,
                            qstat_gap=qstat_gap)
-
     print(f'{len(submitter.commands)} jobs finished.')
-    sys.stdout.flush()
     return
 
 
