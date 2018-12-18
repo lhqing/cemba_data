a9e5bf0865392152dc133d2e8d2e4531b46e111b
lhqing <liuhq@ucsd.edu>
2018-12-18 10:35:30 -0800

    add new function

---
+++ cemba_data/local/mc/prepare_cluster_profile.py
@@ -0,0 +1,14 @@
+"""
+Input:
+- a dict json file. key: cluster ID, value: list of cell IDs
+- metadata contain file path
+- output dir
+
+1. gather all file paths based on metadata
+2. merge allc
+3. extract CG strand merge allc
+4. bigwig for CG and CH
+5. file path summary file
+
+TODO: write this cluster profile preparing
+"""
---
+++ cemba_data/tools/simulation.py
@@ -0,0 +1,18 @@
+"""
+a. generate a bed file mimic pseudo-reads covering genome
+Input:
+- Read length distribution
+- Read number distribution
+- Genome coverage distribution (or for single cell, 2)
+Output:
+a bed file
+
+b. Simulate pseudo-profiles based a given bed
+Input:
+- ALLC file path, the high coverage ALLC profile to simulate from
+- The bed file
+Output:
+pseudo-ALLC
+
+TODO: write simulation function
+"""
