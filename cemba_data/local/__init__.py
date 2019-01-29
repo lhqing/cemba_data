5a531e88213c60c901886ab0c2fc8098841f2865
lhqing <liuhq@ucsd.edu>
2019-01-28 19:54:16 -0800

    clean

--- cemba_data/local/__init__.py
+++ cemba_data/local/__init__.py
@@ -3,5 +3,12 @@ This package contain inhouse script only intend to use for very specific purpose
 
 Package file structure:
 
-
+atac/
+    Code for deal with atac data, from snapR
+mc/
+    Code for deal with inhouse mC data
+rna/
+    Code for deal with RNA data and all Allen's other data like ISH dataset
+qsub.py
+    inhouse qsubmitter for submitting SGE qsub jobs
 """
--- cemba_data/mapping/__init__.py
+++ cemba_data/mapping/__init__.py
@@ -1,4 +1,6 @@
 """
+Mapping related functions
+
 Currently mapping only take normal snmC-seq or snNOMe-seq data.
 TODO: add stats summary notebook and auto-render to generate plots and stats
 TODO: process snmCT-seq data
