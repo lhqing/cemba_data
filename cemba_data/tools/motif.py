9a4acdc1e60e808b58a702aa9a89365f7e837c78
lhqing <liuhq@ucsd.edu>
2019-02-28 22:22:32 +0800

    clean code

--- cemba_data/tools/motif.py
+++ cemba_data/tools/motif.py
@@ -14,7 +14,38 @@ log.addHandler(logging.NullHandler())
 
 
 def get_motif_bed(motif_file_path, genome, out_prefix, non_overlap=True, non_overlap_kws=None,
-                  score_cutoff=8, bed_out=True, cpu=1, keep_all=True, homer_path=None):
+                  score_cutoff=8, cpu=1, keep_all=True, homer_path=None):
+    """
+    Get a motif bed file [chrom, start, end] from homer motif matrix file, using homer scanMotifGenomeWide.pl
+    The output is bgzip & tabixed, and usually non-overlapped,
+    because get_motif_rate must have non-overlap input.
+    See homer doc here: http://homer.ucsd.edu/homer/motif/genomeWideMotifScan.html
+
+    Parameters
+    ----------
+    motif_file_path
+        Path to motif matrix file, homer format.
+        See doc here: http://homer.ucsd.edu/homer/motif/creatingCustomMotifs.html
+    genome
+        this directly goes to homer, accept genome name like 'mm10' or a fasta file path
+    out_prefix
+        prefix of the output, suffix will be added automatically
+    non_overlap
+        If true, will use bedtools merge to merge all overlap regions
+    non_overlap_kws
+        dict key word parameters pass to bedtools merge
+    score_cutoff
+        homer motif match score used to filter motif regions
+    cpu
+        Number of CPU to use in homer
+    keep_all
+        Homer parameter, keep ALL sites, even ones that overlap
+    homer_path
+        Path to homer executable files
+    Returns
+    -------
+
+    """
     if homer_path is not None:
         os.environ['PATH'] += ':' + homer_path
     # check if homer function can be found
@@ -25,20 +56,14 @@ def get_motif_bed(motif_file_path, genome, out_prefix, non_overlap=True, non_ove
                                 'add homer to PYTHONPATH or provide '
                                 '/path/to/homer/bin/ in homer_path parameter')
 
-    _bed_out = ' '
-    if bed_out:
-        _bed_out = ' -bed'
-
     _keep_all = ' '
     if keep_all:
         _keep_all = ' -keepAll'
-
-    cmd = f'scanMotifGenomeWide.pl {motif_file_path} {genome}{_bed_out}{_keep_all} -p {cpu} -int'
+    cmd = f'scanMotifGenomeWide.pl {motif_file_path} {genome}{_keep_all} -bed -p {cpu} -int'
 
     score_count = collections.defaultdict(int)
     total_line_count = 0
     pass_line_count = 0
-
     out_path = out_prefix + '.motif.bed'
     with open(out_path, 'w') as f:
         process = subprocess.Popen(shlex.split(cmd),
@@ -51,7 +76,6 @@ def get_motif_bed(motif_file_path, genome, out_prefix, non_overlap=True, non_ove
             # chr10   3101570 3101579 Lhx1(Homeobox)  7       -
             # chr10   3103413 3103422 Lhx1(Homeobox)  6       +
             # chr10   3103415 3103424 Lhx1(Homeobox)  6       -
-
             # This function only keep chrom, start, end.
             ll = line.strip().split('\t')
             score = int(ll[4])
@@ -85,11 +109,14 @@ def get_motif_bed(motif_file_path, genome, out_prefix, non_overlap=True, non_ove
         raise OSError(p.stderr)
 
 
-def check_bed_nonoverlap(bed_path):
+def _check_bgz_bed_non_overlap(bed_path):
+    if not bed_path.endswith('.gz'):
+        raise ValueError('Bed file should be bgzip/tabixed to use this function.')
+
     cur_chrom = 'NOTACHROM'
     last_end = -1
     line_number = 0
-    with open(bed_path) as f:
+    with open_gz(bed_path) as f:
         for line in f:
             line_number += 1
             chrom, start, end, *_ = line.strip('\n').split('\t')
@@ -106,7 +133,7 @@ def check_bed_nonoverlap(bed_path):
     return True
 
 
-def get_tabix_chroms(file_path):
+def _get_tabix_chroms(file_path):
     process = subprocess.run(['tabix', file_path, '-l'], check=True,
                              stdout=subprocess.PIPE, encoding='utf8')
 
@@ -123,8 +150,8 @@ def _get_non_overlap_motif_rate(allc_path, bed_path, chrom_size_path, mc_types):
         raise FileNotFoundError(f'Bed file {bed_path} do not have .tbi index')
 
     ref_chrom_dict = parse_chrom_size(chrom_size_path)
-    allc_chroms = get_tabix_chroms(allc_path)
-    bed_chroms = get_tabix_chroms(bed_path)
+    allc_chroms = _get_tabix_chroms(allc_path)
+    bed_chroms = _get_tabix_chroms(bed_path)
     use_chroms = (set(ref_chrom_dict.keys()) & set(allc_chroms) & set(bed_chroms))
 
     mc_count_dict = {mc_type: 0 for mc_type in mc_types}
@@ -174,29 +201,58 @@ def _get_non_overlap_motif_rate(allc_path, bed_path, chrom_size_path, mc_types):
     return pd.DataFrame(records)
 
 
-def batch_get_motif_rate(cell_allc_table, bed_path, chrom_size_path,
-                         out_path, mc_type, cpu=10):
-    cell_allc_paths = pd.read_table(cell_allc_table, index_col=0, squeeze=True, header=None)
+def batch_get_motif_rate(allc_path_table, bed_path, chrom_size_path,
+                         out_path, mc_types, cpu=10):
+    """
+    Map a list of ALLC files into a non-overlap bed file.
+    But only count the overall mC and cov across the whole bed file.
+
+    Parameters
+    ----------
+    allc_path_table
+        Two column table without header. The 1st is file id, the 2nd is the allc path
+    bed_path
+        Path of the non-overlap bed file
+    chrom_size_path
+        Path of the UCSC chrom size file
+    out_path
+        Path of the output table
+    mc_types
+        mC context pattern(s)
+    cpu
+        Number of CPU to use
+
+    Returns
+    -------
+
+    """
+    if not _check_bgz_bed_non_overlap(bed_path):
+        raise ValueError('Bed file is not non-overlap, this function only take non-overlap intervals. '
+                         'Use bedtools merge to merge that. Or set non_overlap=True in get_motif_bed function.')
+    cell_allc_paths = pd.read_table(allc_path_table, index_col=0, squeeze=True, header=None)
     if not isinstance(cell_allc_paths, pd.Series):
-        raise ValueError('cell_allc_table is malformed. It should only contain 2 columns without header line, '
-                         'the 1st is cell id, the 2nd is the allc path.')
+        raise ValueError('allc_path_table is malformed. It should only contain 2 columns without header line, '
+                         'the 1st is file id, the 2nd is the allc path.')
+
+    if isinstance(mc_types, str):
+        mc_types = [mc_types]
 
     with ProcessPoolExecutor(max_workers=cpu) as executor:
         future_result = {executor.submit(_get_non_overlap_motif_rate,
                                          allc_path=allc_path,
                                          bed_path=bed_path,
                                          chrom_size_path=chrom_size_path,
-                                         mc_type=mc_type): cell_id
-                         for cell_id, allc_path in cell_allc_paths.iteritems()}
+                                         mc_types=mc_types): file_id
+                         for file_id, allc_path in cell_allc_paths.iteritems()}
         records = []
         for future in as_completed(future_result):
-            cell_id = future_result[future]
+            file_id = future_result[future]
             try:
                 result = future.result()
-                result['cell_id'] = cell_id
+                result['file_id'] = file_id
                 records.append(result)
             except Exception as exc:
-                log.info('%r generated an exception: %s' % (cell_id, exc))
+                log.info('%r generated an exception: %s' % (file_id, exc))
     total_result = pd.concat(records, sort=True, ignore_index=True)
     total_result.to_csv(out_path, sep='\t', index=None)
     return
