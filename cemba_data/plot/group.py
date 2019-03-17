a67f98bddcd2ecd45ae48ead8c9d37ac3e654ab0
lhqing <liuhq@ucsd.edu>
2019-03-17 22:56:33 +0800

    init group level plots

---
+++ cemba_data/plot/group.py
@@ -0,0 +1,19 @@
+"""
+Group visualization
+
+Input:
+- Tidy Data: row is cell/group, groupby columns (x, y), groupby orders (x, y)
+- Matrix Data: row is x|y, col is y|x. groupby and groupby orders provide separately
+- Dendrogram: provided separately
+
+Plot Type:
+- Dotplot/Matrixplot: tidy data, single axes, multi visual map (size, color, marker)
+- Heatmap: matrix data, single ax, single visual map
+- Stack-violin: matrix data, multi axes, single visual map
+- TracePlot: matrix data, multi axes, single visual map
+
+Appendix:
+- dendrogram
+- category color blocks: ax, orientation, order
+- category brackets
+"""
