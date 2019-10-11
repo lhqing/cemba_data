c6f5ffaa970f0539dfa77dc124dcd386e345c0bd
lhqing <liuhq@ucsd.edu>
2019-10-10 18:21:18 -0700

    change to bedtools cmd

--- cemba_data/atac/bigwig.py
+++ cemba_data/atac/bigwig.py
@@ -1,8 +1,6 @@
 import subprocess
 from concurrent.futures import ProcessPoolExecutor, as_completed
 
-import pybedtools
-
 
 def fragment_to_bigwig(fragment_bed_path,
                        chrom_size_path,
@@ -18,33 +16,39 @@ def fragment_to_bigwig(fragment_bed_path,
     print('Sort fragment')
     out_sort_path = str(out_bw_path) + 'temp.sort.bed'
     try:
+        # sort fragment bed
         subprocess.run(f'zcat {fragment_bed_path} | sort -k1,1 -k2,2n '
                        f'-S {sort_mem} --parallel {int(sort_cpu)} > {out_sort_path}',
                        stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding='utf8',
                        check=True, shell=True)
+
         # count reads
         p = subprocess.run(['wc', '-l', out_sort_path],
                            stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding='utf8',
                            check=True)
         total_reads = int(p.stdout.split(' ')[0])
         scale = scale_factor / total_reads
+
+        print('Calculate COV')
+        out_bg_path = output_prefix + '.bg'
+        subprocess.run(f'bedtools genomecov -i {out_sort_path} '
+                       f'-g {chrom_size_path} -scale {scale} -bg > {out_bg_path}',
+                       stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding='utf8',
+                       check=True, shell=True)
+
+        print('Generate bigwig')
+        subprocess.run(['bedGraphToBigWig', out_bg_path, chrom_size_path, out_bw_path],
+                       stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding='utf8', check=True)
+
+        if remove_bg:
+            subprocess.run(['rm', '-f', out_bg_path, out_sort_path], check=True)
+        else:
+            subprocess.run(['mv', '-f', out_sort_path], check=True)
+
     except subprocess.CalledProcessError as e:
         print(e.stderr)
         raise e
 
-    print('Calculate COV')
-    frag_sorted_bed = pybedtools.BedTool(out_sort_path)
-    cov_bed = frag_sorted_bed.genome_coverage(scale=scale, bg=True, g=chrom_size_path)
-    out_bg_path = output_prefix + '.bg'
-    cov_bed.saveas(str(out_bg_path))
-
-    print('Generate bigwig')
-    subprocess.run(['bedGraphToBigWig', out_bg_path, chrom_size_path, out_bw_path],
-                   stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding='utf8', check=True)
-    if remove_bg:
-        subprocess.run(['rm', '-f', out_bg_path, out_sort_path], check=True)
-    else:
-        subprocess.run(['mv', '-f', out_sort_path], check=True)
     return out_bw_path
 
 
