d343cc2e6cff242d9c94ae93535e5c60189335c9
lhqing <liuhq@ucsd.edu>
2019-10-23 10:58:35 -0700

    update picard command

--- cemba_data/mapping/bismark_bam_qc.py
+++ cemba_data/mapping/bismark_bam_qc.py
@@ -32,7 +32,8 @@ def bismark_bam_qc(output_dir, config):
         # command
         filter_cmd = f'samtools view -b -h -q {mapq_threshold} -o {filter_bam} {bismark_bam_path}'
         sort_cmd = f'samtools sort -o {sort_bam} --threads 2 {filter_bam}'
-        dedup_cmd = f'picard MarkDuplicates I={sort_bam} O={final_bam} M={dedup_matrix} REMOVE_DUPLICATES=true'
+        dedup_cmd = f'picard -Xms4g -Xmx4g MarkDuplicates ' \
+                    f'-I {sort_bam} -O {final_bam} -M {dedup_matrix} -REMOVE_DUPLICATES true'
         cleaning_cmd = f'rm -f {bismark_bam_path} {sort_bam} {filter_bam}'
         command = ' && '.join([filter_cmd, sort_cmd, dedup_cmd, cleaning_cmd])
         records.append([uid, index_name, read_type, final_bam])
