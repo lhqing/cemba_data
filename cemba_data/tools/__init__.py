4d7872c9e912c931ad219e2a78baa1b140d82760
lhqing <liuhq@ucsd.edu>
2020-05-05 18:11:37 -0700

    cleaning

--- cemba_data/tools/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Package file structure:
-
-hdf5/
-    dealing with HDF5 based datasets, such as netcdf file or anndata file.
-
-"""
--- cemba_data/tools/allc/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-ALLC format related functions
-"""
-from .allc_to_bigwig import allc_to_bigwig
-from .bam_to_allc import call_methylated_sites, batch_call_methylated_sites
-from .merge_allc import merge_allc_files
-from ._open import open_allc, open_bam, open_gz
-from .utilities import standardize_allc, profile_allc, map_to_region, tabix_allc
-from .extract_allc import extract_allc
