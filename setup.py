d3314b9134b37955b0e1f432de69d05c678f0c0c
lhqing <liuhq@ucsd.edu>
2020-09-29 11:31:22 -0700

    add version

--- setup.py
+++ setup.py
@@ -1,27 +1,29 @@
 from setuptools import setup, find_packages
 
-yap_version = '1.0.0'
-
 setup(
-    name='yap',
-    version=yap_version,
+    name='cemba-data',
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
     author='Hanqing Liu',
     author_email='hanliu@salk.edu',
-    packages=find_packages(),
     description='Pipelines for single nucleus methylome and multi-omic dataset.',
     long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/lhqing/cemba_data',
+    license='MIT',
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=find_packages(exclude=('doc',)),
     include_package_data=True,
-    install_requires=['pandas>=1.0', 'numpy', 'seaborn', 'matplotlib', 'papermill', 'dnaio', 'pysam'],
     package_data={
         '': ['*.txt', '*.tsv', '*.csv', '*.fa', '*Snakefile', '*ipynb']
     },
+    install_requires=['pandas>=1.0', 'numpy', 'seaborn', 'matplotlib', 'papermill', 'dnaio', 'pysam'],
     entry_points={
         'console_scripts': ['yap=cemba_data.__main__:main',
                             'yap-internal=cemba_data._yap_internal_cli_:internal_main'],
     }
 )
-
-if __name__ == '__main__':
-    f = open("cemba_data/__init__.py", 'w')
-    f.write(f"__version__ = '{yap_version}'\n")
-    f.close()
