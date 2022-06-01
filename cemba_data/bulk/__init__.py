dcfe16a680f1332db5d157c66d7422f95ebc90ce
jksr <jksr.tw@gmail.com>
2022-06-01 15:45:30 -0700

    add allc merging method from multiple groups

--- cemba_data/bulk/__init__.py
+++ cemba_data/bulk/__init__.py
@@ -1,3 +1,4 @@
 from .mc_bulk import prepare_mc_bulk
 from .atac_bulk import prepare_atac_bulk
 from .mct_bulk import merge_mct_cluster_bam
+from .mc_bulk_multigroup.mc_bulk_multigroup import merge_bulk_multigroup
